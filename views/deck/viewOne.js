5499db29877b7a38866d36a75ddf3c17e7abc8f8
Lee Morgan <me@leemorgan.io>
2023-06-30 15:29:36 -0400

    Add buttons to flip card. Flip cards on arrow button press.

--- views/deck/viewOne.js
+++ views/deck/viewOne.js
@@ -1,3 +1,24 @@
+let currentCard = 0;
+let cards = document.getElementById("cards").children;
+
 let flip = (elem)=>{
     elem.classList.toggle("flipped");
+}
+
+let changeCard = (num)=>{
+    currentCard += num;
+    if(currentCard < 0) currentCard = 0;
+    if(currentCard > cards.length - 1) currentCard = cards.length - 1;
+
+    for(let i = 0; i < cards.length; i++){
+        cards[i].style.display = "none";
+    }
+
+    cards[currentCard].style.display = "flex";
+}
+
+document.onkeyup = (event)=>{
+    if(event.keyCode === 32) flip(cards[currentCard]);
+    if(event.keyCode === 39) changeCard(1);
+    if(event.keyCode === 37) changeCard(-1);
 }
